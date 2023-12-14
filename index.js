c915f4221da6d3b5bb7c424d5d6d85bbd719f5de
Alisher Uralov <aliwerdev@gmail.com>
2023-12-15 00:42:19 +0500

    fads

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const QarzerBot = require("./bot/qarzer");
 
 const mongoURI = "mongodb+srv://aliwerdev:1234@cluster0.qu6p9ff.mongodb.net/qarzer";
-const TG_TOKEN = "6874089411:AAH_OK8QXoLMrOGmMfS-FSiAMeF8BMe82j8";
+const TG_TOKEN = "6874089411:AAHODLWxmg9Cm1rGE7k1HEgZkzygSGlm-aY";
 
 mongoose
   .connect(mongoURI)
