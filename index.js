64b3e2a824c8bc956025b016cf22950291d584b8
Alisher Uralov <aliwerdev@gmail.com>
2024-07-26 11:42:39 +0500

    fsd

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const mongoose = require("mongoose");
-const app = require("express")();
 const cors = require("cors");
 const cachegoose = require("recachegoose");
 const QarzerBot = require("./bot/qarzer");
-const routes = require("./admin/routes");
 const { mongo_url, tg_token } = require("./config");
 
 cachegoose(mongoose, {
   engine: "memory",
 });
-
-app.use(cors());
-app.use("/api", routes);
 
 mongoose
   .connect(mongo_url)
